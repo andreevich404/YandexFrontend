d6b3ca9194ecc5ccaf3ad3f08bca4580b6a37879
Ivan Andreevich <56831140+andreevich404@users.noreply.github.com>
2024-05-18 09:19:37 +0300

    change base url

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL = 'https://api.andreevich404.nomoredomainswork.ru'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
