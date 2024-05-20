5eab52be16fbd28444d3eed03227f52e40c1230c
Ivan Andreevich <56831140+andreevich404@users.noreply.github.com>
2024-05-20 16:56:01 +0300

    fix route api

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.andreevich404.nomoredomainswork.ru'
+export const BASE_URL = 'https://api.andreevich404.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
