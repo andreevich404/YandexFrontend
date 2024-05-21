ccaa69e8372f0f0c1eafadfc79f5b25bb1a92249
Ivan Andreevich <56831140+andreevich404@users.noreply.github.com>
2024-05-21 15:54:45 +0300

    fix some errors and bugs

--- app/api/config.js
+++ app/api/config.js
@@ -1,7 +1,7 @@
-export const BASE_URL = 'https://api.andreevich404.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://api.andreevich404.nomoredomainswork.ru/api';
 
 export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/login`,
-  me: `${BASE_URL}/me`,
-}
+	games: `${BASE_URL}/games`,
+	auth: `${BASE_URL}/auth/login`,
+	me: `${BASE_URL}/me`,
+};
--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,10 @@
-import { AuthForm } from "../components/AuthForm/AuthForm"
-import Styles from "./login.module.css";
+import { AuthForm } from '../components/AuthForm/AuthForm';
+import Styles from './login.module.css';
 
-export default function LoginPage () {
-  return (
-    <main className={Styles['main']}>
-      <AuthForm />
-    </main>
-  )
+export default function LoginPage() {
+	return (
+		<main className={Styles['main']}>
+			<AuthForm />
+		</main>
+	);
 }
